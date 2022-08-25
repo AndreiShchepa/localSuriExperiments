dee0c633954aa6857295f3dcc9288658ba2fe8e9
Shchepa <andrewbandit228@gmail.com>
2022-08-25 09:52:19 +0200

    fix bug with grep on ryzlink server

--- statSuri.py
+++ statSuri.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import json, sys, os, re
+import json, sys, os, re, subprocess
 from colorama import Fore
 
 def printRules(jsonEl, valBySort):
     try:
-        num = int(sys.argv[len(sys.argv) - 1])
+        num = int(sys.argv[3])
     except:
         num = 10
     
     i = 1
     for el in jsonEl:
-        print(Fore.RED + str(i) + ". " + str(valBySort) + ": " + str(el[valBySort]) + Fore.WHITE)
-        os.system("grep -E ^alert " + str(sys.argv[2]) + " |  grep -E sid:[\ ]{0,1}" + str(el["signature_id"]))
+        with open(sys.argv[4], "a") as f:
+            print("\n####" + str(i) + ". " + str(valBySort).upper() + ": " + str(el[valBySort]) + "####", file=f)
+        os.system("grep -E ^alert " + str(sys.argv[2]) + " |  grep -E \"sid:[\ ]{0,1}" + str(el["signature_id"]) + "\" >> " + sys.argv[4])
 
         i += 1
         if i > num:
@@ -34,7 +35,10 @@ dictRules = {
         "max ticks" : "ticks_max"
     }
 
+os.system("rm " + sys.argv[4])
 for el in jsonStr:
-    print(20*"*" + el['sort'])
+    with open(sys.argv[4], "a") as f:
+        print(50*"*" + el['sort'], file=f)
     printRules(el["rules"], dictRules[el['sort']])
-    print(30*"*" + '\n')
+    with open(sys.argv[4], "a") as f:
+        print('\n' + 30*"*" + '\n', file=f)
